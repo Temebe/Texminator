a387ea6214ef76503996fd6eaee569f0c6dddaf9
Sebastian Aksamit <seboado@gmail.com>
2020-04-11 16:02:01 +0200

    init

---
+++ cmake-build-debug/CMakeFiles/Texminator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Texminator.dir/main.cpp.o"
+  "Texminator"
+  "Texminator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Texminator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
